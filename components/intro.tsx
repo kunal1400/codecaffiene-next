b054698008509f57058954ef56b416e3917b9713
kunal1400 <kunal.malviya@team.telstra.com>
2023-02-23 19:51:43 +0530

    Blog template

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,27 +1,16 @@
-import { CMS_NAME, CMS_URL } from '../lib/constants'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        <Link href="/">
+          <Image src="https://codecaffiene.com/wp-content/uploads/2023/02/codecaffiene-logo.png" alt="codecaffiene logo" width={255} height={83} />
+        </Link>        
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and{' '}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
+        Technologies used: <i>NextJs</i> + <i>TypeScript</i> + <i>Tailwind CSS</i>
       </h4>
     </section>
   )
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -5,14 +5,13 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import Container from '../../components/container'
 import PostBody from '../../components/post-body'
 import MoreStories from '../../components/more-stories'
-import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import SectionSeparator from '../../components/section-separator'
 import Layout from '../../components/layout'
 import PostTitle from '../../components/post-title'
 import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
-import { CMS_NAME } from '../../lib/constants'
+import Intro from '../../components/intro'
 
 export default function Post({ post, posts, preview }) {
   const router = useRouter()
@@ -25,16 +24,14 @@ export default function Post({ post, posts, preview }) {
   return (
     <Layout preview={preview}>
       <Container>
-        <Header />
+        <Intro/>
         {router.isFallback ? (
           <PostTitle>Loading…</PostTitle>
         ) : (
           <>
             <article>
               <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
+                <title>{post.title}</title>
                 <meta
                   property="og:image"
                   content={post.featuredImage?.node.sourceUrl}
